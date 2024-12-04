fbaafe3016911da46a0df6aa618c673345504d92
AlvarFred <alvarf2016@outlook.com>
2024-12-04 20:16:46 +0100

    Finished day 4 part 2

--- 2024/4/main.py
+++ 2024/4/main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import re
 
-regex = "XMAS|SAMX"
-
 def part1(file):
     lines = []
     finds = 0
@@ -10,16 +8,13 @@ def part1(file):
         lines.append(list(line.strip()))
         finds += len(re.findall("XMAS",line))
         finds += len(re.findall("SAMX",line))
-    # print(lines)
-    matrix = np.array(lines)
-    diags = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1,matrix.shape[1])]
 
-    # Now back to the original array to get the upper-left-to-lower-right diagonals,
-    # starting from the right, so the range needed for shape (x,y) was y-1 to -x+1 descending.
+    matrix = np.array(lines)
+    
+    diags = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1,matrix.shape[1])]
     diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1,-matrix.shape[0],-1))
 
-    # Another list comp to convert back to Python lists from numpy arrays,
-    # so it prints what you requested.
+
     for n in diags:
         line = ''.join(n.tolist())
         finds += len(re.findall("XMAS",line))
@@ -31,25 +26,28 @@ def part1(file):
         finds += len(re.findall("SAMX",line))
     return finds
 
-# -1,-1 +1,-1
-#-1,+1 +1,+1
-#
+
 def part2(file):
     lines = []
+    total = 0
+    
     for line in file:
         lines.append(line)
-
+    # Don't need to search the first and last row and column 
     for i in range(1,len(lines)-1):
-        for j in range(1,len(lines[i])-1):
+        for j in range(1,len(lines[i])-2):
             if lines[i][j] == "A":
-                diagonal = lines[i-1][j-1] + 'A' + lines[j+1][i+1]
-                print(diagonal)    
+                diagonal = lines[i-1][j-1] + 'A' + lines[i+1][j+1]
+                if diagonal == "SAM" or diagonal == "MAS":
+                    diagonal = lines[i+1][j-1] + 'A' + lines[i-1][j+1]
+                    if diagonal == "SAM" or diagonal == "MAS":
+                        total += 1
+    return total 
             
-
 def main():
     file = open("input.txt", "r")
     print("Part 1: ", part1(file))
-    file = open("example.txt", "r")
+    file = open("input.txt", "r")
     print("Part 2: ", part2(file))
 
 if __name__ == "__main__":
