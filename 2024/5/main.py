6d97609f4d045ae284a1e751668d1adfd55c7d79
AlvarFred <alvarf2016@outlook.com>
2024-12-05 15:20:31 +0100

    Tried a solution for day 5

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -1,7 +1,7 @@
 
 def part1(file):
-    rules = []
     updates = []
+    solution = []
     update_part = False
     for line in file:
         if line == '\n': 
@@ -10,16 +10,46 @@ def part1(file):
         if update_part:
             updates.append(line)
         else:
-            rules.append(line)
-    print(rules)
-    print(updates)
-    pass
+            numbers = line.strip().split("|")
+            index0 = None
+            if numbers[1] not in solution:
+                print(numbers[1], " is added")
+                # indexes[numbers[1]] = len(solution)
+                solution.append(numbers[1])
+            if numbers[0] in solution:
+                index0 = solution.index(numbers[0])
+                if index0 > solution.index(numbers[1]):
+                    del solution[index0]
+                    print(numbers[0], " deleted at: ", index0)
+                else: continue
+            print(numbers[0], " is added")
+
+            solution.insert(solution.index(numbers[1]), numbers[0])
+            # indexes[numbers[0]] = indexes[numbers[1]]
+            # indexes[numbers[1]] += 1
+
+            # print (numbers)
+
+    total = 0
+    for update in updates:
+        update_list = update.strip().split(',')
+        # print(update_list)
+        if is_ordered_subsequence(update_list, solution):
+            total += int(update_list[len(update_list) // 2])
+        # print (update_list, is_ordered_subsequence(update_list, solution))
+    print(solution)
+    return total
+
+def is_ordered_subsequence(sub, main):
+    it = iter(main)
+    return all(item in it for item in sub)
+    
 
 def part2(file):
     pass
 
 def main():
-    file = open("example.txt", "r")
+    file = open("input.txt", "r")
     print("Part 1: ", part1(file))
     file = open("input.txt", "r")
     print("Part 2: ", part2(file))
